c7695dc12c042c78b51bfc5017cadef9c8d35e1d
Michael Mackrell <michaeljmackrell@gmail.com>
2019-02-28 14:03:07 -0500

    test to begin work on welcome slide

---
+++ src/welcome_slide.js
@@ -0,0 +1,11 @@
+import { React } from "react";
+import { ReactDOM } from "react-dom"
+
+
+export const WelcomeSlide = () => {
+
+    return(
+            <p>hello and welcome</p>
+        );
+    
+};
