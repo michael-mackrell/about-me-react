352ace0223f8cbcda760b3b6c7780dfed178c204
Michael Mackrell <michaeljmackrell@gmail.com>
2019-03-01 00:09:43 -0500

    Updates

---
+++ precache-manifest.b9bebfff4c4cffbc8550a4a8f8dd2f6a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "202e5ad59d319955eae433b91fa25512",
+    "url": "/static/media/pl.202e5ad5.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1cd475778730a71e8bc6d29bdfffc765",
+    "url": "/static/media/ice_lake.1cd47577.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "902317175f1560b68f5d",
+    "url": "/static/js/main.90231717.chunk.js"
+  },
+  {
+    "revision": "aef4366c1d6ac7f54422",
+    "url": "/static/js/2.aef4366c.chunk.js"
+  },
+  {
+    "revision": "902317175f1560b68f5d",
+    "url": "/static/css/main.929ad495.chunk.css"
+  },
+  {
+    "revision": "92d3533109e5c7a4b2cec0a0ce62a002",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/about-me-react/precache-manifest.59cae181643abf08b8c1653590d7a714.js"
+  "/precache-manifest.b9bebfff4c4cffbc8550a4a8f8dd2f6a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/about-me-react/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
